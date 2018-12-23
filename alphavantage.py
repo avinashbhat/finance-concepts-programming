17c5b256b8df7ec8583aa3c69dbc6b6929608bc7
avinashbhat <avin.bhat@yahoo.com>
2018-12-23 20:48:32 +0530

    Created get_time_series_daily wrapper from alpha vantage API

--- alphavantage.py
+++ alphavantage.py
@@ -1,12 +1,9 @@
 import requests
 
-def get_time_series_daily(symbol, apikey, downloadable, function="TIME_SERIES_DAILY", outputsize="compact"):
-	datatype = 'json'
-	if downloadable:
-		datatype = 'csv'
-	payload = {'function': function, 'symbol': symbol, 'outputsize': outputsize, 'apikey': apikey, 'datatype': datatype}
+def get_time_series_daily(symbol, apikey, function="TIME_SERIES_DAILY", outputsize="compact"):
+	payload = {'function': function, 'symbol': symbol, 'outputsize': outputsize, 'apikey': apikey}
 	r = requests.get('https://www.alphavantage.co/query', params=payload)
-	return r
+	return r.json()
 
 
-print(get_time_series_daily(symbol='MSFT', apikey='PQPNGOENWG0YERJ0', downloadable=False))
+print(get_time_series_daily(symbol='MSFT', apikey='PQPNGOENWG0YERJ0'))
