2808bd18b145349a9e8da1a9919d5ac83a36e4be
avinashbhat <avin.bhat@yahoo.com>
2018-12-23 18:10:19 +0530

    Added two util functions - to generate random numbers

---
+++ util.py
@@ -0,0 +1,12 @@
+import random
+import numpy
+
+# Util function to generate random number between two indices
+def generate_random_number(startingIndex, endingIndex):
+	return random.randint(startingIndex, endingIndex)
+
+
+# Util function to generate a random matrix, provided the starting and 
+# ending indices 
+def generate_random_matrix(startingIndex, endingIndex, xrows, yrows):
+	return numpy.random.randint(startingIndex, endingIndex, (xrows, yrows))
