18fd4b0058a78a767a8bca605a1df8ce9e795cc1
avinashbhat <avin.bhat@yahoo.com>
2018-12-25 09:30:29 +0530

    takes data as parameter

--- logarithmic_returns.py
+++ logarithmic_returns.py
@@ -1,11 +1,8 @@
-from util import convert_dictionary_to_numpy
-from alphavantage import get_time_series_daily_adjusted
 from matplotlib import pyplot as plt
 import math
 
 
-def log_return(symbol, apikey):
-	stock = convert_dictionary_to_numpy(get_time_series_daily_adjusted(symbol=symbol, apikey=apikey))
+def log_return(stock):
 	stock['logReturn'] = [math.log(float(value)/float(stock['5. adjusted close'][index-1])) for index, value in enumerate(stock['5. adjusted close'])]
 	stock.iloc[0,8] = math.nan
 	stock['logReturn'].plot(figsize=(8, 5))
--- simple_returns.py
+++ simple_returns.py
@@ -1,11 +1,8 @@
-from util import convert_dictionary_to_numpy
-from alphavantage import get_time_series_daily_adjusted
 from matplotlib import pyplot as plt
 import math
 
 
-def simple_return(symbol, apikey):
-	stock = convert_dictionary_to_numpy(get_time_series_daily_adjusted(symbol=symbol, apikey=apikey))
+def simple_return(stock):
 	stock['simpleReturn'] = [(float(value)/float(stock['5. adjusted close'][index-1])) - 1 for index, value in enumerate(stock['5. adjusted close'])]
 	stock.iloc[0,8] = math.nan
 	stock['simpleReturn'].plot(figsize=(8, 5))
