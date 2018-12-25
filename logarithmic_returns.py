1d3cce8ddb40caff4855d2feb6e63216610826c0
avinashbhat <avin.bhat@yahoo.com>
2018-12-25 09:24:08 +0530

    Converted to functions

--- logarithmic_returns.py
+++ logarithmic_returns.py
@@ -3,19 +3,14 @@ from alphavantage import get_time_series_daily_adjusted
 from matplotlib import pyplot as plt
 import math
 
-PG = convert_dictionary_to_numpy(get_time_series_daily_adjusted(symbol='PG', apikey='PQPNGOENWG0YERJ0'))
-# print(type(PG['5. adjusted close'][1]))
 
-PG['logReturn'] = [math.log(float(value)/float(PG['5. adjusted close'][index-1])) for index, value in enumerate(PG['5. adjusted close'])]
-PG.iloc[0,8] = math.nan
-# print(PG.head())
-
-PG['logReturn'].plot(figsize=(8, 5))
-plt.show()
-
-# Multiply mean by 250 as trading days are 250
-meanForLogReturn = PG['logReturn'].mean() * 250
-logReturnPercentage = round(meanForLogReturn * 100, 2)
-
-print("Mean: " + str(meanForLogReturn))
-print("Percentage: " + str(logReturnPercentage) + "%")
+def log_return(symbol, apikey):
+	stock = convert_dictionary_to_numpy(get_time_series_daily_adjusted(symbol=symbol, apikey=apikey))
+	stock['logReturn'] = [math.log(float(value)/float(stock['5. adjusted close'][index-1])) for index, value in enumerate(stock['5. adjusted close'])]
+	stock.iloc[0,8] = math.nan
+	stock['logReturn'].plot(figsize=(8, 5))
+	plt.show()
+	# Multiply mean by 250 as trading days are 250
+	meanForLogReturn = stock['logReturn'].mean() * 250
+	logReturnPercentage = round(meanForLogReturn * 100, 2)
+	return (meanForLogReturn, logReturnPercentage)
