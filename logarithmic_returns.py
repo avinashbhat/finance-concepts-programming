7989ff56d99515e899dc0ea3d957ebccb3c0d2d9
avinashbhat <avin.bhat@yahoo.com>
2018-12-26 08:50:51 +0530

    added functions in returns files

--- logarithmic_returns.py
+++ logarithmic_returns.py
@@ -1,8 +1,9 @@
 from matplotlib import pyplot as plt
 import math
 
-
-def log_return(stock):
+# Function to return log return value for adjusted close values given of a particular stock
+# Takes in series and returns a series
+def log_return_mean_percentage(stock):
 	stock['logReturn'] = [math.log(float(value)/float(stock['5. adjusted close'][index-1])) for index, value in enumerate(stock['5. adjusted close'])]
 	stock.iloc[0,8] = math.nan
 	stock['logReturn'].plot(figsize=(8, 5))
@@ -11,3 +12,8 @@ def log_return(stock):
 	meanForLogReturn = stock['logReturn'].mean() * 250
 	logReturnPercentage = round(meanForLogReturn * 100, 2)
 	return (meanForLogReturn, logReturnPercentage)
+
+def log_return_list(stockAdjustedClose):
+	logReturn = [math.log(float(value)/float(stockAdjustedClose[index-1])) for index, value in enumerate(stockAdjustedClose)]
+	logReturn[0] = math.nan
+	return logReturn
--- simple_returns.py
+++ simple_returns.py
@@ -1,8 +1,9 @@
 from matplotlib import pyplot as plt
 import math
 
-
-def simple_return(stock):
+# Function to return simple return value for adjusted close values given of a particular stock
+# Takes in series and returns a series
+def simple_return_mean_percentage(stock):
 	stock['simpleReturn'] = [(float(value)/float(stock['5. adjusted close'][index-1])) - 1 for index, value in enumerate(stock['5. adjusted close'])]
 	stock.iloc[0,8] = math.nan
 	stock['simpleReturn'].plot(figsize=(8, 5))
@@ -11,3 +12,8 @@ def simple_return(stock):
 	meanForSimpleReturn = stock['simpleReturn'].mean() * 250
 	simpleReturnPercentage = round(meanForSimpleReturn * 100, 2)
 	return (meanForSimpleReturn, simpleReturnPercentage)
+
+def simple_return_list(stockAdjustedClose):
+	simpleReturn = [math.log(float(value)/float(stockAdjustedClose[index-1])) for index, value in enumerate(stockAdjustedClose)]
+	simpleReturn[0] = math.nan
+	return simpleReturn
