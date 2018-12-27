e8af57470fbdf539e8e37926e4ceac92ccb55455
avinashbhat <avin.bhat@yahoo.com>
2018-12-27 07:45:24 +0530

    risk calculation based on variance

---
+++ demo.py
@@ -0,0 +1,16 @@
+from portfolio_rate_of_return import getPortfolioRateofReturn
+from util import convert_dictionary_to_numpy
+from alphavantage import get_time_series_daily_adjusted
+from constants import apikey
+
+# stockData = convert_dictionary_to_numpy(get_time_series_daily_adjusted(symbol='PG', apikey=apikey))
+
+# mean, percentage = simple_return(stockData)
+# print(mean)
+# print(percentage)
+
+# mean, percentage = log_return(stockData)
+# print(mean)
+# print(percentage)
+
+print(getPortfolioRateofReturn(['PG', 'F'], [0.5, 0.5]))
---
+++ risk_calculation.py
@@ -0,0 +1,25 @@
+from logarithmic_returns import log_return_list
+from util import convert_dictionary_to_numpy
+from alphavantage import get_time_series_daily_adjusted
+from constants import apikey
+import pandas
+
+def calculate_risk(tickers):
+	data = dict()
+	for symbol in tickers:
+		data[symbol] = convert_dictionary_to_numpy(get_time_series_daily_adjusted(symbol=symbol, apikey=apikey))['5. adjusted close']
+
+	logReturnData = dict()
+	for symbol in data:
+		logReturnData[symbol] = log_return_list(data[symbol])
+
+	data = pandas.DataFrame.from_dict(logReturnData)
+
+	meanValues = [data[ticker].mean() for ticker in data]
+	stdValues = [data[ticker].std() for ticker in data]
+
+	# Multiply with 250 to accomodate the trading days
+	meanValues = [values*250 for values in meanValues]
+	variance = [(values*250)**0.5 for values in stdValues]
+
+	return (meanValues, variance)
